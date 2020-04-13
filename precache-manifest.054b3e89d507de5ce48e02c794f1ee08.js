9157fbef113c80d915675323613c4dea72308347
Aidar Ibatullin <amazing.space.invader@gmail.com>
2020-04-13 10:44:23 +0300

    Updates

---
+++ precache-manifest.054b3e89d507de5ce48e02c794f1ee08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cdf8256341c4a37678dc7c4ef8c800",
+    "url": "/irregular-verbs/index.html"
+  },
+  {
+    "revision": "30ce48d200b1b38e105e",
+    "url": "/irregular-verbs/static/js/2.d0cf7c2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/irregular-verbs/static/js/2.d0cf7c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f4bd5c7de3d48ffe5b",
+    "url": "/irregular-verbs/static/js/main.de3c743e.chunk.js"
+  },
+  {
+    "revision": "e91c7ead1e3b22586c90",
+    "url": "/irregular-verbs/static/js/runtime-main.f55580bd.js"
+  }
+]);
